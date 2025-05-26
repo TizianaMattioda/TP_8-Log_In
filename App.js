af92a4c6de79a78c2f68f28fa55d902354950baa
TizianaMattioda <48464113@est.ort.edu.ar>
2025-05-26 10:11:21 -0300

    TP Terminado(?

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import {SafeAreaView, SafeAreaProvider, StyleSheet, Button, View, Text, Alert, Image, TextInput } from 'react-native';
+import React from 'react';
 
 const profilePhoto = {
   uri: 'https://preview.redd.it/hyacine-bp-icon-v0-a1upcim6r01f1.jpeg?width=640&crop=smart&auto=webp&s=53b71229254a96ee2928bd2e626e08140211c41d' 
@@ -8,18 +9,46 @@ const normalGif = {
   uri: 'https://i.redd.it/bc9c02trbv1f1.gif' 
 };
 
-
 export default function App() {
+  const [email, onChangeEmail] = React.useState('');
+  const [password, onChangePassword] = React.useState('');
+  
+
   return (
     <View style={styles.container}>
+        <StatusBar
+          backgroundColor="#61dafb"
+        />
+
       <View style={styles.header}>
         <Text style={styles.title}>Login App (Hyacine)</Text>
       </View>
       
       <Image source={normalGif} style={styles.banner} />
       <Image source={profilePhoto} style={styles.avatar} />
+
+      <TextInput
+          style={styles.input}
+          onChangeText={onChangeEmail}
+          placeholder="a@b.com"
+          value={email}
+        />
+      <TextInput
+          style={styles.input}
+          onChangeText={onChangePassword}
+          placeholder ="password"
+          value={password}
+      />
       
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button style={styles.boton}
+          title="Ingresar!"
+          color= "#85dcf3"
+          onPress={() => Alert.alert('Witness, the will of humanity!')}
+        />
+      
+      <Text style={styles.texto}>¿Olvidaste la contraseña?</Text>
+      <Text style={styles.texto}>Crear cuenta</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -63,5 +92,24 @@ const styles = StyleSheet.create({
     borderColor: '#fff',
     backgroundColor: '#eee',
   },
+  boton: {
+    width: 100
+  },
 
+  texto: {
+    fontSize: 15,
+    marginTop: 10  
+  }
+  
+  ,input: {
+    height: 40,
+    width: 300,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    color: "#7f7f7f",
+    borderColor: "#dd3087",
+    borderRadius: 3,
+    borderWidth: 2
+  },
 });
